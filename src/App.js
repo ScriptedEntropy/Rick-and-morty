e1158a46c442d356a9541a5c31b99b20bad871a1
ScriptedEntropy <ramiro.job1@gmail.com>
2023-12-19 10:56:29 -0300

    states added and stuff

--- src/App.js
+++ src/App.js
@@ -1,33 +1,50 @@
-import './App.css'
-import Card from './components/Card.jsx'
-import Cards from './components/Cards.jsx'
-import SearchBar from './components/SearchBar.jsx'
-import characters, { Rick } from './data.js'
+import styles from './App.module.css'
+import Cards from './components/Cards/Cards.jsx'
+import Nav from './components/Nav/Nav.jsx'
+import {useState} from 'react'
+
 
 function App () {
+
+  const [characters, setCharacters] = useState([]);
+
+function onSearch(character) {
+  fetch(`https://rickandmortyapi.com/api/character/${character}`)
+     .then((response) => response.json())
+     .then((data) => {
+        if (data.name) {
+           setCharacters((oldChars) => [...oldChars, data]);
+        } else {
+           window.alert('No hay personajes con ese ID');
+        }
+     });
+}
+
+const onClose = (id)=>{
+  const filtered = characters.filter((char)=> char.id !== Number(id))
+  setCharacters(filtered)
+}
+
   return (
-    <div className='App' style={{ padding: '25px' }}>
-      <div>
-        <Card
-          name={Rick.name}
-          species={Rick.species}
-          gender={Rick.gender}
-          image={Rick.image}
-          onClose={() => window.alert('Emulamos que se cierra la card')}
+    
+    <div className={styles.App}>
+
+      <div >
+        <Nav
+        onSearch={onSearch}
         />
       </div>
-      <hr />
-      <div>
+
+     <div>
         <Cards
           characters={characters}
+          onClose={onClose}
         />
       </div>
+
       <hr />
-      <div>
-        <SearchBar
-          onSearch={(characterID) => window.alert(characterID)}
-        />
-      </div>
+      
+
     </div>
   )
 }
