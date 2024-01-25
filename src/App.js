8f02f7e74acf689cc45f368977a8bf7bb1bc8491
ScriptedEntropy <ramiro.job1@gmail.com>
2024-01-24 21:52:05 -0300

    logIn added, I spent an unreasonable amount of time with this sh*t due to an error. At least I learned some stuff lol

--- src/App.js
+++ src/App.js
@@ -3,14 +3,30 @@ import About from './components/About/About.jsx'
 import Cards from './components/Cards/Cards.jsx'
 import Nav from './components/Nav/Nav.jsx'
 import Detail from './components/Detail/Detail.jsx'
-import {useState} from 'react'
-import { Route, Routes } from 'react-router-dom'
+import Form from './components/Form/Form.jsx'
+import {useState, useEffect} from 'react'
+import { Route, Routes, useLocation, useNavigate } from 'react-router-dom'
 
 
 
 function App () {
 
   const [characters, setCharacters] = useState([]);
+
+  const navigate = useNavigate();
+  const [access, setAccess] = useState(false)
+  const username = 'test@test.com'
+  const password = 'password'
+  function login(userData) {
+    if (userData.password === password && userData.username === username) {
+       setAccess(true);
+       navigate('/home');
+    }
+ }
+
+ useEffect(() => {
+  !access && navigate('/');
+}, [access, navigate]);
 
 function onSearch(character) {
   fetch(`https://rickandmortyapi.com/api/character/${character}`)
@@ -29,11 +45,16 @@ const onClose = (id)=>{
   setCharacters(filtered)
 }
 
+const location = useLocation();
+const showNav = location.pathname !== '/'
+
   return (
     
     <div className={styles.App}>
-  <Nav onSearch={onSearch}/>
+  {showNav && <Nav onSearch={onSearch}/>}
+  
   <Routes>
+  <Route path='/' element={<Form login={login}/>}/>
   <Route path='/home' element={<Cards characters={characters} onClose={onClose}/>}/>
   <Route path='/about' element={<About/>}/>
   <Route path= "/detail/:id" element={<Detail/>}/>
